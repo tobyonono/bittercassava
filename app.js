49c9728cbce35f228f35d3771c89242e172deb35
tobyonono <ononop@tcd.ie>
2020-08-09 23:29:50 +0100

    Backend Functionality + Updated Frontend(Video Approach)

--- app.js
+++ app.js
@@ -19,6 +19,40 @@ var redirect_uri = 'http://localhost:4000/callback'; // Your redirect uri
 var app = express();
 var clientSide = express();
 
+var server = require("http").Server(app);
+var io = require("socket.io")(server, {});
+var shortid = require('shortid');
+var broadcastingAccessToken;
+var broadCastingRefreshToken;
+
+var imgsrc;
+var currentUserName;
+var profiler;
+var songName;
+var artistName;
+var albumName;
+var progress;
+var songLength;
+var songURL;
+var isPlaying;
+var uri;
+var deviceID;
+var playbackArray = [];
+var i = 0;
+var playbackJson = {"playback":[]};
+
+var jsonPushData = {
+  userName: currentUserName,
+  albumArt: imgsrc,
+  songName: songName,
+  artistName: artistName,
+  albumName: albumName,
+  songProgress: progress,
+  songLength: songLength,
+  songURL: songURL,
+  isPlaying: isPlaying
+};
+
 // view engine setup
 
 
@@ -111,10 +145,6 @@ app.get('/callback', function(req, res) {
         };
 
         // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
-          console.log("Server Response...");
-          console.log(body);
-        });
 
         // we can also pass the token to the browser to make requests from there
         res.redirect('/home/#' +
@@ -156,8 +186,130 @@ app.get('/refresh_token', function(req, res) {
   });
 });
 
+app.get('/isBroadcastingUser', function(req, res)
+{
+  return( req.userID === '1158091471' ? true : false);
+/**
+  if (userID ==='1158091471')
+  {
+    var options = {
+      url: 'https://api.spotify.com/v1/me/player',
+      headers: { 'Authorization': 'Bearer ' + access_token },
+      json: true
+    };
+
+    // use the access token to access the Spotify Web API
+    request.get(options, function(error, response, body) {
+
+    });
+  }
+  **/
+
+
+
+});
+
+/*
+function refreshBroadcaster(refreshToken)
+{
+  var refToken = refreshToken;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refToken
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      broadcastingAccessToken = body.access_token;
+
+    }
+  });
+
+  var options = {
+    url: 'https://api.spotify.com/v1/me/player',
+    headers: { 'Authorization': 'Bearer ' + broadcastingAccessToken },
+    json: true
+  };
+
+  // use the access token to access the Spotify Web API
+  request.get(options, function(error, response, body) {
+
+    playbackJson = {
+      imgsrc: body.item.album.images[0].url,
+      songName: body.item.name,
+      artistName: body.item.artists[0].name,
+      albumName: body.item.album.name,
+      progress: body.progress_ms,
+      songLength: body.timestamp,
+      isPlaying: body.is_playing,
+      uri: body.item.uri,
+      trackID: body.item.id
+    }
+
+    console.log(body);
+
+  });
+}
+*/
+
+
+
+//socket stuff to notify client there's a change in playbackInfo
+
+io.sockets.on("connection", function(socket)
+{
+    var clientName = shortid.generate();
+    console.log("client was connected, name = " + clientName);
+
+    socket.on("getMyName", function()
+    {
+        socket.emit("onGetMyName", { name: clientName });
+    });
+
+    socket.on("sendMyNameToAllClients", function()
+    {
+        socket.broadcast.emit("onSendMyNameToAllClients", { name: clientName });
+        console.log(clientName);
+    });
+
+    socket.on("BroadcasterPlayback", function(data)
+    {
+      if(playbackJson.playback.length === 0)
+      {
+        playbackJson.playback.push(data);
+        if(playbackJson.playback[0].isPlaying == true)
+        {
+          socket.broadcast.emit("playSong", data);
+        }
+      }
+      else if(playbackJson.playback[0].trackID !== data.trackID)
+      {
+        socket.broadcast.emit("playSong", data);
+        playbackJson.playback[0] = data;
+      }
+      else if(playbackJson.playback[0].trackID === data.trackID)
+      {
+        if((data.isPlaying === true) && (playbackJson.playback[0].isPlaying === false))
+        {
+          socket.broadcast.emit("resumeSong", data);
+          playbackJson.playback[0] = data;
+        }
+        else if((data.isPlaying === false) && (playbackJson.playback[0].isPlaying === true))
+        {
+          socket.broadcast.emit("pauseSong", data);
+          playbackJson.playback[0] = data;
+        }
+      }
+    });
+});
+
 const port = 4000;
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server listening at ${port}`);
 });
