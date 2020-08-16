143722f167fb81eb6c236865e1c996d31b05325a
tobyonono <ononop@tcd.ie>
2020-08-16 23:12:47 +0100

    Possibly fixed 404 issue?? Changed if statement check for broadcasters

--- app.js
+++ app.js
@@ -180,6 +180,7 @@ app.get('/refresh_token', function(req, res) {
   request.post(authOptions, function(error, response, body) {
     if (!error && response.statusCode === 200) {
       var access_token = body.access_token;
+      console.log("we're in + ", access_token);
       res.send({
         'access_token': access_token
       });
